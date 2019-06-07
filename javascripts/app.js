461b4169d1bfb679ac9c8234ea0f15632f0d8c9a
Juscielma <juscielma.med@gmail.com>
2019-06-07 00:13:13 -0300

    Finalizado

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,60 +3,101 @@ var rover = {
   direcao: "n",
   x: 0,
   y: 0,
+  travelLog: []
 }
 
-function turnLeft(robo) {
-
-  if (robo.direcao == "l") {
-    return robo.direcao = "n";
-  }
-  if (robo.direcao == "n") {
-    return robo.direcao = "o";
-  }
-  if (robo.direcao == "o") {
-    return robo.direcao = "s";
-  }
-  if (robo.direcao == "s") {
-    return robo.direcao = "l";
-  }
-
-  // switch (robo.direcao) {
-  //   case "l":
-  //     robo.direcao = "n";
-  //     break;
-  //   case "n":
-  //     robo.direcao = "o";
-  //     break;
-  // }
-}
-
-function turnRight(robo) {
-  if (robo.direcao == "l") {
-    return robo.direcao = "s";
-  }
-  if (robo.direcao == "s") {
-    return robo.direcao = "o";
-  }
-  if (robo.direcao == "o") {
-    return robo.direcao = "n";
-  }
-  if (robo.direcao == "n") {
-    return robo.direcao = "l";
+function turnLeft() {
+  switch (rover.direcao) {
+    case "l":
+      rover.direcao = "n";
+      console.log("O rover mudou para a direcao: " + rover.direcao);
+      break;
+    case "n":
+      rover.direcao = "o";
+      console.log("O rover mudou para a direcao: " + rover.direcao);
+      break;
+    case "o":
+      rover.direcao = "s";
+      console.log("O rover mudou para a direcao: " + rover.direcao);
+      break;
+    case "s":
+      rover.direcao = "l";
+      console.log("O rover mudou para a direcao: " + rover.direcao);
+      break;
+    default: console.log("Direcao inexistente")
   }
 }
 
-function moveForward(robo) {
-  if (robo.direcao == "l") {
-    robo.x++;
+function turnRight() {
+  switch (rover.direcao) {
+    case "l":
+      rover.direcao = "s"
+      console.log("O rover mudou para a direcao: " + rover.direcao);
+      break;
+    case "s":
+      rover.direcao = "o";
+      console.log("o rover mudou para a direcao: " + rover.direcao);
+      break;
+    case "o":
+      rover.direcao = "n";
+      console.log("o rover mudou para a direcao: " + rover.direcao);
+      break;
+    case "n":
+      rover.direcao = "l";
+      console.log(" o rover mudou para a direcaoo: " + rover.direcao);
+      break;
+    default: console.log("Direcao inexistente")
   }
-  if (robo.direcao == "s") {
-    robo.y++;
+}
+
+function moveForward() {
+
+  if (rover.direcao === "l" && rover.x >= 0 && rover.x < 9) {
+    rover.x += 1;
+    console.log('x: ' + rover.x + ', y: ' + rover.y);
+
+    rover.travelLog.push(['x: ' + rover.x, 'y: ' + rover.y])
+
   }
-  if (robo.direcao == "o") {
-    robo.x--;
+
+  if (rover.direcao === "s" && rover.y >= 0 && rover.y < 9) {
+    rover.y += 1;
+    console.log('x: ' + rover.x + ', y: ' + rover.y)
+
+    rover.travelLog.push(['x: ' + rover.x, 'y: ' + rover.y])
   }
-  if (robo.direcao == "n") {
-    robo.y--;
+
+  if (rover.direcao === "o" && rover.x > 0 && rover.x <= 9) {
+    rover.x -= 1;
+    console.log('x: ' + rover.x + ', y: ' + rover.y)
+
+    rover.travelLog.push(['x: ' + rover.x, 'y: ' + rover.y])
   }
-  return console.log('x: ' + robo.x + ', y: ' + robo.y)
+
+  if (rover.direcao === "n" && rover.y > 0 && rover.y <= 9) {
+    rover.y -= 1;
+    console.log('x: ' + rover.x + ', y: ' + rover.y)
+
+    rover.travelLog.push(['x: ' + rover.x, 'y: ' + rover.y])
+  }
+}
+
+function commands(string) {
+  for (let i = 0; i <= string.length - 1; i += 1) {
+    switch (string[i]) {
+      case "f":
+        moveForward();
+        console.log("moveForward foi chamado")
+        break;
+      case "r":
+        turnRight();
+        console.log("turnRight foi chamado")
+        break;
+      case "l":
+        turnLeft();
+        console.log("turnLeft foi chamado")
+        break;
+      default: console.log("comando errado")
+    }
+  }
 }
